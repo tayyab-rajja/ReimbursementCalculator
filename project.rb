f24854ad63e2db5e9bc49594c195395d99576d13
tayyab-rajja <tayyabraja316@gmail.com>
2022-12-29 18:44:40 +0500

    Project Class
      * Added Date parser to project dates
      * Added correct date formats to dates

--- project.rb
+++ project.rb
@@ -2,8 +2,8 @@ class Project
   attr_reader :start_date, :end_date, :city_type
 
   def initialize(start_date, end_date, city_type)
-    @start_date = start_date
-    @end_date = end_date
+    @start_date = Date.strptime(start_date, "%m/%d/%y")
+    @end_date = Date.strptime(end_date, "%m/%d/%y")
     @city_type = city_type
   end
 end
